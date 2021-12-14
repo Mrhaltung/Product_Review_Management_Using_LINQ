ca37838d8f15dd4db11b723411c3fe8845b8f765
Mrhaltung <dipeshchandekar99@gmail.com>
2021-12-14 12:38:27 +0530

    Retrieve top 3 records from the list rating

---
+++ Product_Review_Management_Using_LINQ/Management.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Product_Review_Management_Using_LINQ
+{
+    public class Management
+    {
+        public void RetrieveTop3Records(List<ProductReview> review)
+        {
+            var recordData = (from products in review
+                              orderby products.Rating descending
+                              select products).Take(3);
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("Product Id : " + list.ProductId + " || User Id : " + list.UserId + " || Rating : " + list.Rating + " || Review : " + list.Review + " || Is Like : " + list.isLike);
+            }
+        }
+    }
+}
--- Product_Review_Management_Using_LINQ/Program.cs
+++ Product_Review_Management_Using_LINQ/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data;
 using System.Collections.Generic;
 
 namespace Product_Review_Management_Using_LINQ
@@ -10,7 +9,7 @@ namespace Product_Review_Management_Using_LINQ
         {
             Console.WriteLine("*****Product Review Management Program*****\n");
 
-            List<ProductReview> productReviewlist = new List<ProductReview>()
+            List<ProductReview> productlist = new List<ProductReview>()
             {
                 new ProductReview() { ProductId = 1, UserId = 1, Rating = 4.9, Review = "Excelent", isLike = true },
                 new ProductReview() { ProductId = 1, UserId = 1, Rating = 4.9, Review = "Excelent", isLike = true },
@@ -38,11 +37,20 @@ namespace Product_Review_Management_Using_LINQ
                 new ProductReview() { ProductId = 9, UserId = 3, Rating = 4.5, Review = "VeryGood", isLike = true },
                 new ProductReview() { ProductId = 4, UserId = 5, Rating = 4.1, Review = "Good    ", isLike = true }
             };
-           
-            foreach (var list in productReviewlist)
+
+            foreach (var list in productlist)
             {
                 Console.WriteLine("Product Id : " + list.ProductId + " || User Id : " + list.UserId + " || Rating : " + list.Rating + " || Review : " + list.Review + " || Is Like : " + list.isLike);
             }
+            Console.WriteLine("------------------------------------------------------------------------------------------------");
+
+            Management management = new Management();
+
+            management.RetrieveTop3Records(productlist);
+
+            Console.WriteLine("------------------------------------------------------------------------------------------------");
+
+
         }
     }
 }
